77e17f5b55a23dff0b6972218b610daa30cd2735
chlovers <chloie_day@hotmail.com>
2025-02-24 15:30:17 -0400

    Initial commit

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    Animator animator;
+    bool spin;
+
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        spin = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            if (spin == false)
+            {
+                animator.SetBool("spin", true);
+                spin = true;
+                Debug.Log("spin started");
+            }
+
+             else if (spin==true)
+            {
+                animator.SetBool("spin", false);
+                spin = false;
+                Debug.Log("spin stoped");
+            }
+        }
+
+
+
+
+    }
+}
